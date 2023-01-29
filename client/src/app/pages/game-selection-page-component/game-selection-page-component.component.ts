a2e3f459f4cfb1fcd68063985896a1f98866a087
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-01-29 12:54:24 -0500

    précédent et suivant marche pas pour l'instant

--- client/src/app/pages/game-selection-page-component/game-selection-page-component.component.ts
+++ client/src/app/pages/game-selection-page-component/game-selection-page-component.component.ts
@@ -6,11 +6,38 @@ import { Component } from '@angular/core';
     styleUrls: ['./game-selection-page-component.component.scss'],
 })
 export class GameSelectionPageComponent {
+    games = [
+        { title: 'Game 1', image: 'https://upload.wikimedia.org/wikipedia/en/4/4e/Angry_Birds_logo.svg', level: 'easy', configuration: false },
+        { title: 'Game 2', image: 'game2.jpg', level: 'medium', configuration: true },
+        { title: 'Game 3', image: 'game3.jpg', level: 'hard', configuration: false },
+        { title: 'Game 1', image: 'game1.jpg', level: 'easy', configuration: false },
+        { title: 'Game 2', image: 'game2.jpg', level: 'medium', configuration: true },
+        { title: 'Game 3', image: 'game3.jpg', level: 'hard', configuration: false },
+        { title: 'Game 2', image: 'game2.jpg', level: 'medium', configuration: true },
+        { title: 'Game 3', image: 'game3.jpg', level: 'hard', configuration: false },
+    ];
+
     gameTitle1 = 'Angry Birds';
     gameTitle2 = 'DbD';
     gameTitle3 = 'Pokemon';
     gameTitle4 = 'Fortnite';
     gameImage1 = 'https://upload.wikimedia.org/wikipedia/en/4/4e/Angry_Birds_logo.svg';
+
+    currentPage = 0;
+    pageSize = 4;
+    lastPage = Math.ceil(this.games.length / this.pageSize) - 1;
+
+    onBack() {
+        if (this.currentPage > 0) {
+            this.currentPage--;
+        }
+    }
+
+    onNext() {
+        if (this.currentPage < this.lastPage) {
+            this.currentPage++;
+        }
+    }
 }
 
 /* import { Component, OnInit } from '@angular/core';
