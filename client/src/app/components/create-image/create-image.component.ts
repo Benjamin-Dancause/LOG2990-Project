08ab686a943d2b701c9914533179d99574d62189
ArcaneAmzek <charles.arsenault@polymtl.ca>
2023-01-30 22:56:35 -0500

    Style des items et ajout de fenetres pour l'upload de fichier

--- client/src/app/components/create-image/create-image.component.ts
+++ client/src/app/components/create-image/create-image.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-create-image',
@@ -6,7 +7,39 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./create-image.component.scss'],
 })
 export class CreateImageComponent implements OnInit {
-    constructor() {}
+    @ViewChild('inputDifferentTemplate', { static: true })
+    inputDifferentTemplate: TemplateRef<unknown>;
+    @ViewChild('inputSameTemplate', { static: true })
+    inputSameTemplate: TemplateRef<unknown>;
+    constructor(public dialog: MatDialog) {}
 
     ngOnInit(): void {}
+
+    showInputDifferent(): void {
+        this.dialog.open(this.inputDifferentTemplate, {
+            width: '500px',
+            height: '250px',
+        });
+    }
+    showInputSame(): void {
+        this.dialog.open(this.inputSameTemplate, {
+            width: '450px',
+            height: '200px',
+        });
+    }
+
+    validateImage(fileEvent: Event): void {
+        if (!(fileEvent.target instanceof HTMLInputElement) || !fileEvent.target.files) {
+            console.error('No files detected');
+            return;
+        }
+
+        const file = fileEvent.target.files[0];
+        if (!file.name.endsWith('.bmp')) {
+            console.error('File must be in BMP format');
+        }
+    }
+}
+export interface HTMLInputEvent extends Event {
+    target: (HTMLInputElement & EventTarget) | null;
 }
