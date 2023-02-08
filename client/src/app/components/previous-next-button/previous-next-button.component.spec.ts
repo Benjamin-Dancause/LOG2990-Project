263b0e43e9dbbb51f7450781c37b97f1d351d20f
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-02-07 20:25:59 -0500

    tests pour previous-next done

--- client/src/app/components/previous-next-button/previous-next-button.component.spec.ts
+++ client/src/app/components/previous-next-button/previous-next-button.component.spec.ts
@@ -3,21 +3,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PreviousNextButtonComponent } from './previous-next-button.component';
 
 describe('PreviousNextButtonComponent', () => {
-  let component: PreviousNextButtonComponent;
-  let fixture: ComponentFixture<PreviousNextButtonComponent>;
+    let component: PreviousNextButtonComponent;
+    let fixture: ComponentFixture<PreviousNextButtonComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PreviousNextButtonComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [PreviousNextButtonComponent],
+        }).compileComponents();
+        fixture = TestBed.createComponent(PreviousNextButtonComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-    fixture = TestBed.createComponent(PreviousNextButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should emit an event when onBack is called', () => {
+        spyOn(component.back, 'emit');
+        component.onBack();
+        expect(component.back.emit).toHaveBeenCalled();
+    });
+
+    it('should emit an event when onNext is called', () => {
+        spyOn(component.next, 'emit');
+        component.onNext();
+        expect(component.next.emit).toHaveBeenCalled();
+    });
 });
