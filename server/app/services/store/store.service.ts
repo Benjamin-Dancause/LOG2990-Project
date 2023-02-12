9539f7659ef7e965e3fdf8034e5bf1866b40a560
inikoloudis <inikoloudis@outlook.com>
2023-02-12 17:17:50 -0500

    store games fixed v2

--- server/app/services/store/store.service.ts
+++ server/app/services/store/store.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { promises as fs } from 'fs';
 
+interface Data {
+    name: string;
+    images: string[];
+}
+
 @Injectable()
 export class StoreService {
     async storeInfo(name: string, relativePaths: string[]): Promise<void> {
-        const info = { name, images: relativePaths };
-        const infoPath = `assets/data/${name}.json`;
-        await fs.writeFile(infoPath, JSON.stringify(info));
+        const infoPath = `assets/data/gamesData.json`;
+        const gameData: Data = { name, images: relativePaths };
+        let gamesData: Data[] = [];
+        try {
+            const gamesContent = await fs.readFile(infoPath, `utf-8`);
+            gamesData = JSON.parse(gamesContent);
+            gamesData.push(gameData);
+            await fs.writeFile(infoPath, JSON.stringify(gamesData));
+        } catch (e) {
+            console.log('No game data found. ' + e);
+        }
     }
 
     async storeImage(name: string, image: string): Promise<string> {
