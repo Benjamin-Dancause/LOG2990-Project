abe106ee86c584c3deb56f67849991cea82da874
Benjamin <benjamindancause298@gmail.com>
2023-02-12 18:59:20 -0500

    countDifference repurposed as getDifference

--- client/src/app/services/difference.service.spec.ts
+++ client/src/app/services/difference.service.spec.ts
@@ -42,26 +42,26 @@ describe('DifferenceService', () => {
     it('should return 1 when there is only 1 difference', () => {
         ctxStub1?.fillRect(0, 0, TEST_VALUE2, TEST_VALUE2);
         const diff = service.findDifference(ctxStub1, ctxStub2, 0);
-        expect(service.countDifference(diff)).toEqual(1);
+        expect(service.getDifference(diff).count).toEqual(1);
     });
 
     it('should return 2 when there is only 2 difference', () => {
         ctxStub1?.fillRect(0, 0, TEST_VALUE2, TEST_VALUE2);
         ctxStub2?.fillRect(TEST_VALUE3, TEST_VALUE3, TEST_VALUE2, TEST_VALUE2);
         const diff = service.findDifference(ctxStub1, ctxStub2, 1);
-        expect(service.countDifference(diff)).toEqual(2);
+        expect(service.getDifference(diff).count).toEqual(2);
     });
 
     it('should return 0 when there is no difference', () => {
         const diff = service.findDifference(ctxStub1, ctxStub2, 1);
-        expect(service.countDifference(diff)).toEqual(0);
+        expect(service.getDifference(diff).count).toEqual(0);
     });
 
     it('should return 1 when 2 differences are close', () => {
         ctxStub1?.fillRect(0, 0, TEST_VALUE2, TEST_VALUE2);
         ctxStub1?.fillRect(TEST_VALUE2, 0, TEST_VALUE2, TEST_VALUE2);
         const diff = service.findDifference(ctxStub1, ctxStub2, TEST_VALUE1);
-        expect(service.countDifference(diff)).toEqual(1);
+        expect(service.getDifference(diff).count).toEqual(1);
     });
 
     it('should return false when every pixel is different', () => {
