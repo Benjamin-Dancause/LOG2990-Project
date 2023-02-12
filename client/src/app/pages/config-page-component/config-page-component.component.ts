20bf2f703c52852baa5b963a5d83c4caee00171f
Benjamin <benjamindancause298@gmail.com>
2023-02-12 00:37:46 -0500

    populate config page

--- client/src/app/pages/config-page-component/config-page-component.component.ts
+++ client/src/app/pages/config-page-component/config-page-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Gamecard } from '@app/classes/gamecard';
+import { CommunicationService } from '@app/services/communication.service';
 
 const PAGE_SIZE = 4;
 
@@ -8,26 +10,23 @@ const PAGE_SIZE = 4;
     styleUrls: ['./config-page-component.component.scss'],
 })
 export class ConfigPageComponent {
-    games = [
-        { title: 'Game 1', image: 'https://i.stack.imgur.com/6umGW.png', level: 'easy', configuration: true },
-        { title: 'Game 2', image: 'https://i.stack.imgur.com/6umGW.png', level: 'hard', configuration: true },
-        { title: 'Game 3', image: 'https://i.stack.imgur.com/6umGW.png', level: 'hard', configuration: true },
-        { title: 'Game 4', image: 'https://i.stack.imgur.com/6umGW.png', level: 'easy', configuration: true },
-        { title: 'Game 5', image: 'https://i.stack.imgur.com/6umGW.png', level: 'easy', configuration: true },
-        { title: 'Game 6', image: 'https://i.stack.imgur.com/6umGW.png', level: 'easy', configuration: true },
-        { title: 'Game 7', image: 'https://i.stack.imgur.com/6umGW.png', level: 'hard', configuration: true },
-        { title: 'Game 8', image: 'https://i.stack.imgur.com/6umGW.png', level: 'hard', configuration: true },
-        { title: 'Game 9', image: 'https://i.stack.imgur.com/6umGW.png', level: 'hard', configuration: true },
-        { title: 'Game 10', image: 'https://i.stack.imgur.com/6umGW.png', level: 'easy', configuration: true },
-        { title: 'Game 11', image: 'https://i.stack.imgur.com/6umGW.png', level: 'hard', configuration: true },
-    ];
+    games: Gamecard[] = [];
 
     startIndex = 0;
     endIndex = 3;
     currentPage = 0;
     pageSize = PAGE_SIZE;
+    lastPage = 0;
 
-    lastPage = Math.ceil(this.games.length / this.pageSize) - 1;
+    constructor(protected communication: CommunicationService) {
+        communication.getAvailableGames().subscribe((gamecards: Gamecard[]) => {
+            for (const gamecard of gamecards) {
+                gamecard.configuration = false;
+            }
+            this.games = gamecards;
+            this.lastPage = Math.ceil(this.games.length / this.pageSize) - 1;
+        });
+    }
 
     get displayedGames() {
         return this.games.slice(this.currentPage * this.pageSize, (this.currentPage + 1) * this.pageSize);
