dae237238cdfe35e596affb6e27136ea43dba8f1
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-02-13 13:28:01 -0500

    quelques erreurs de lint corrigées

--- client/src/app/components/timer/timer.component.ts
+++ client/src/app/components/timer/timer.component.ts
@@ -8,32 +8,31 @@ import { interval, Subscription } from 'rxjs';
     styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit, OnDestroy {
-  min = 0;
-  sec = 0;
-  minutes = '00';
-  seconds = '00';
-  private intervalSubscription: Subscription;
+    min = 0;
+    sec = 0;
+    minutes = '00';
+    seconds = '00';
+    private intervalSubscription: Subscription;
 
-  constructor(private timerService: TimerService) { }
+    constructor(private timerService: TimerService) {}
 
-  ngOnInit(): void {
-    this.intervalSubscription = interval(1000).subscribe(() => {
-      this.timerService.getTime().subscribe(time => {
-        this.min = Math.floor(time / 60);
-        this.sec = time % 60;
-        this.minutes = this.pad(this.min);
-        this.seconds = this.pad(this.sec);
-      });
-    });
-  }
+    ngOnInit(): void {
+        this.intervalSubscription = interval(1000).subscribe(() => {
+            this.timerService.getTime().subscribe((time) => {
+                this.min = Math.floor(time / 60);
+                this.sec = time % 60;
+                this.minutes = this.pad(this.min);
+                this.seconds = this.pad(this.sec);
+            });
+        });
+    }
 
-  ngOnDestroy(): void {
-    this.intervalSubscription.unsubscribe();
-    this.timerService.resetTimer().subscribe();
-  }
+    ngOnDestroy(): void {
+        this.intervalSubscription.unsubscribe();
+        this.timerService.resetTimer().subscribe();
+    }
 
-  pad(value: number) {
-    return value.toString().padStart(2, '0');
-  }
-
+    pad(value: number) {
+        return value.toString().padStart(2, '0');
+    }
 }
