6f61ec367e93c007025805e33e4da3a515471b58
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-03-05 20:27:52 -0500

    on peut rentrer du texte et la scroll bar est fonctionnelle

--- client/src/app/components/text-box/text-box.component.ts
+++ client/src/app/components/text-box/text-box.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-text-box',
     templateUrl: './text-box.component.html',
     styleUrls: ['./text-box.component.scss'],
 })
-export class TextBoxComponent implements OnInit {
-    constructor() {}
+export class TextBoxComponent {
+    @ViewChild('messageArea', { static: true }) messageArea!: ElementRef;
+    messages: { content: string; type: string }[] = [];
+    newMessage: string = '';
 
-    ngOnInit(): void {}
+    scrollToBottom() {
+        setTimeout(() => {
+            this.messageArea.nativeElement.scrollTop = this.messageArea.nativeElement.scrollHeight;
+        }, 0);
+    }
+
+    sendMessage() {
+        if (this.newMessage.length > 0) {
+            const type = 'self';
+            const content = this.newMessage;
+            this.messages.push({ content, type });
+            this.newMessage = '';
+            this.scrollToBottom();
+        }
+    }
 }
