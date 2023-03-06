510eacc1320ff726eddfa7b2f3fb9bb8152b4ed8
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-03-05 19:49:13 -0500

    ajout d'un component text-box

---
+++ client/src/app/components/text-box/text-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-text-box',
+    templateUrl: './text-box.component.html',
+    styleUrls: ['./text-box.component.scss'],
+})
+export class TextBoxComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
