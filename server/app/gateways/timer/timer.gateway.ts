cb2eb03fa7903a648765d22f8b94f78a50732ee1
Jérémy Langevin <jeremy.langevin@polymtl.ca>
2023-03-08 18:46:19 -0500

    Timer rework done

--- server/app/gateways/timer/timer.gateway.ts
+++ server/app/gateways/timer/timer.gateway.ts
@@ -6,35 +6,50 @@ export class TimerGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer() server: Server;
   
   private timers = new Map<string, number>();
+  private intervals = new Map<string, NodeJS.Timeout>();
   
   handleConnection(client: Socket) {
-    const roomId = client.handshake.query.roomId;
+    const roomId = client.handshake.query.id as string;
+    console.log(roomId);
     if(roomId){
       client.join(roomId);
       const time: number = this.getTimeFromRoom(roomId);
       this.timers.set(roomId, time);
-      this.emitTimeToRoom(roomId, time);
+      const intervalId = setInterval( () => {
+        this.updateTimer(roomId);
+      }, 1000);
+      this.intervals.set(roomId, intervalId);
     }
   }
-
+  
   handleDisconnect(@ConnectedSocket() client: Socket) {
-    const roomId = client.handshake.query.roomId;
+    const roomId = client.handshake.query.roomId as string;
     if(roomId){
       client.leave(roomId);
       this.timers.delete(roomId);
+      clearInterval(this.intervals.get(roomId));
+      this.intervals.delete(roomId);
     }
   }
+  
+  updateTimer(roomId: string) {
+    this.timers.set(roomId, this.timers.get(roomId) + 1);
+        this.emitTimeToRoom(roomId, this.timers.get(roomId));
+  }
 
-  @SubscribeMessage('get-time')
-  onTimer(client: any, data: any){
-    this.time++;
-    this.server.emit('timer', this.time);
+  emitTimeToRoom(roomId: string, time: number) {
+    this.server.to(roomId).emit('timer', time);
+  }
+  
+  getTimeFromRoom(roomId: string): number {
+    return this.timers.get(roomId) || 0;
   }
 
   @SubscribeMessage('reset-timer')
-  onReset(client: any, data: any){
-    this.time = 0;
-    this.server.emit('reset', this.time);
+  onReset(client: Socket, roomId: string){
+    const time = 0;
+    this.timers.set(roomId, time);
+    this.emitTimeToRoom(roomId, time);
   }
 
 }
