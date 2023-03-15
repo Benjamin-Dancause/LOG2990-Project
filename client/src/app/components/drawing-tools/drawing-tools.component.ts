41c986d0820a56d6265259abe90beac2c59c35fa
ArcaneAmzek <charles.arsenault@polymtl.ca>
2023-03-15 14:34:56 -0400

    Ajout des commandes clavier

--- client/src/app/components/drawing-tools/drawing-tools.component.ts
+++ client/src/app/components/drawing-tools/drawing-tools.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
 @Component({
@@ -46,13 +46,25 @@ export class DrawingToolsComponent implements OnInit {
     deleteRight(): void {
         this.drawingService.deleteRight();
     }
+    @HostListener('document:keydown.control.z')
     undo(): void {
         this.drawingService.undoAction();
     }
+    @HostListener('document:keydown.control.shift.z')
     redo(): void {
         this.drawingService.redoAction();
     }
     test(): void {
         this.drawingService.test();
+    }
+
+    onKeyDown(event: KeyboardEvent) {
+        if (event.ctrlKey && event.key === 'z') {
+            if (event.shiftKey) {
+                this.undo();
+            } else {
+                this.redo();
+            }
+        }
     }
 }
