517508ae6ea387f97d259699eff836594b2b5410
inikoloudis <inikoloudis@outlook.com>
2023-03-16 00:51:45 -0400

    added changes in topbar where we can now see our name and opponent name in 1v1

--- client/src/app/components/top-bar/top-bar.component.ts
+++ client/src/app/components/top-bar/top-bar.component.ts
@@ -9,10 +9,19 @@ export class TopBarComponent implements OnInit {
     @Input() name: string;
     @Input() single: boolean;
     userName: string;
+    opponent: string = '';
 
     ngOnInit() {
         const storedUserName = sessionStorage.getItem('userName');
         this.userName = storedUserName ? storedUserName : '';
         this.name = (sessionStorage.getItem('gameTitle') as string) || '';
+
+        if (!this.single) {
+            if (this.userName === (sessionStorage.getItem('gameMaster') as string)) {
+                this.opponent = sessionStorage.getItem('joiningPlayer') as string;
+            } else {
+                this.opponent = sessionStorage.getItem('gameMaster') as string;
+            }
+        }
     }
 }
