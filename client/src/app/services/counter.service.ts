47db96612e4c3086e5d1329596ed6918c6d09817
inikoloudis <inikoloudis@outlook.com>
2023-03-17 11:40:39 -0400

    added math.ceil for the winCondition in 1v1

--- client/src/app/services/counter.service.ts
+++ client/src/app/services/counter.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CommunicationService } from './communication.service';
 import { WaitingRoomService } from './waiting-room.service';
@@ -6,33 +6,37 @@ import { WaitingRoomService } from './waiting-room.service';
 @Injectable({
     providedIn: 'root',
 })
-export class CounterService {
+export class CounterService implements OnInit {
     counter: number = 0;
     counter2: number = 0;
     winCondition: number;
+    gameMode: string;
     allDiffsSubscription: Subscription;
 
     constructor(public waitingRoomService: WaitingRoomService, private communicationService: CommunicationService) {}
 
+    ngOnInit(): void {}
+
     initializeCounter(): void {
+        const gameTitle: string = sessionStorage.getItem('gameTitle') as string;
+        this.gameMode = sessionStorage.getItem('gameMode') as string;
+        this.setWinCondition(this.gameMode, gameTitle);
         this.waitingRoomService.socket.on('counter-update', (counterInfo: { counter: number; player1: boolean }) => {
             const playerName: string = sessionStorage.getItem('userName') as string;
             const gameMaster: string = sessionStorage.getItem('gameMaster') as string;
-            const gameMode: string = sessionStorage.getItem('gameMode') as string;
-            const gameTitle: string = sessionStorage.getItem('gameTitle') as string;
             const isPlayer1: boolean = gameMaster === playerName;
-            this.setWinCondition(gameMode, gameTitle);
+
             console.log('LINE 17 IN COUNTER SERVICE: ' + isPlayer1);
-            if (!(gameMode === 'solo') && isPlayer1 !== counterInfo.player1) {
+            if (!(this.gameMode === 'solo') && isPlayer1 !== counterInfo.player1) {
                 this.counter2 = counterInfo.counter;
-                if(this.counter2 === this.winCondition) {
+                if (this.counter2 === this.winCondition) {
                     this.waitingRoomService.sendVictoriousPlayer(isPlayer1);
                 }
                 console.log('Your opponent found a difference !');
                 console.log('Opponent count is: ' + this.counter2);
             } else {
                 this.counter = counterInfo.counter;
-                if(this.counter === this.winCondition) {
+                if (this.counter === this.winCondition) {
                     this.waitingRoomService.sendVictoriousPlayer(isPlayer1);
                 }
 
@@ -53,17 +57,16 @@ export class CounterService {
 
     setWinCondition(gameMode: string, gameTitle: string) {
         this.allDiffsSubscription = this.communicationService.getDiffAmount(gameTitle).subscribe((totalDiff: number) => {
-            if(gameMode !== 'solo') {
-                this.winCondition = totalDiff / 2;
-            }
-            else {
+            if (gameMode !== 'solo') {
+                this.winCondition = Math.ceil(totalDiff / 2);
+            } else {
                 this.winCondition = totalDiff;
             }
+            console.log('LINE 25 THIS IS THE 1v1 WIN COND: ' + this.winCondition);
         });
     }
 
     ngOnDestroy() {
         this.allDiffsSubscription.unsubscribe();
     }
-
 }
