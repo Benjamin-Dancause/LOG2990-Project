2480c708f43baad9ce9c8ae2340207604ba94523
ArcaneAmzek <charles.arsenault@polymtl.ca>
2023-03-17 16:14:19 -0400

    Tests pour le drawing component

--- client/src/app/components/drawing/drawing.component.spec.ts
+++ client/src/app/components/drawing/drawing.component.spec.ts
@@ -5,13 +5,14 @@ import { DrawingComponent } from './drawing.component';
 
 describe('DrawingComponent', () => {
     let component: DrawingComponent;
-    let drawingSpy: jasmine.SpyObj<DrawingService>;
+    let drawingServiceSpy: jasmine.SpyObj<DrawingService>;
     let fixture: ComponentFixture<DrawingComponent>;
 
     beforeEach(async () => {
+        drawingServiceSpy = jasmine.createSpyObj(['register', 'registerBackground', 'start', 'execute', 'end', 'unregister']);
         await TestBed.configureTestingModule({
             declarations: [DrawingComponent],
-            providers: [{ provide: DrawingService, useValue: drawingSpy }],
+            providers: [{ provide: DrawingService, useValue: drawingServiceSpy }],
         }).compileComponents();
 
         fixture = TestBed.createComponent(DrawingComponent);
@@ -25,7 +26,43 @@ describe('DrawingComponent', () => {
     it('should register on creation', () => {
         spyOn(component, 'ngAfterViewInit').and.callThrough();
         component.ngAfterViewInit();
-        expect(drawingSpy.register).toHaveBeenCalled();
+        expect(drawingServiceSpy.register).toHaveBeenCalled();
+        expect(drawingServiceSpy.registerBackground).toHaveBeenCalled();
+    });
+    it('should start the drawing when the mouse is clicked', () => {
+        spyOn(component, 'onMouseDown').and.callThrough();
+        const position = 100;
+        const mockClick = new MouseEvent('mousedown', {
+            clientX: position,
+            clientY: position,
+        });
+        component.onMouseDown(mockClick);
+        expect(drawingServiceSpy.start).toHaveBeenCalled();
+    });
+    it('should execute the action when the mouse moves', () => {
+        spyOn(component, 'onMouseMove').and.callThrough();
+        const position = 100;
+        const mockClick = new MouseEvent('mousemove', {
+            clientX: position,
+            clientY: position,
+        });
+        component.onMouseMove(mockClick);
+        expect(drawingServiceSpy.execute).toHaveBeenCalled();
+    });
+    it('should stop drawing when the mouse is not clicked', () => {
+        spyOn(component, 'onMouseUp').and.callThrough();
+        const position = 100;
+        const mockClick = new MouseEvent('mouseup', {
+            clientX: position,
+            clientY: position,
+        });
+        component.onMouseUp(mockClick);
+        expect(drawingServiceSpy.end).toHaveBeenCalled();
+    });
+    it('should unregister on destroy', () => {
+        spyOn(component, 'ngOnDestroy').and.callThrough();
+        component.ngOnDestroy();
+        expect(drawingServiceSpy.unregister).toHaveBeenCalled();
     });
     afterEach(() => {
         fixture.destroy();
