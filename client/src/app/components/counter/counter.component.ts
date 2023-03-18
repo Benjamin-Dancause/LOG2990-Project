cf25203ef686e61c96ac4cffeec636a12fd4045f
inikoloudis <inikoloudis@outlook.com>
2023-03-17 21:43:31 -0400

    fixed bug where client side counters would reset too late and cause premature game end

--- client/src/app/components/counter/counter.component.ts
+++ client/src/app/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { CounterService } from '@app/services/counter.service';
 
 @Component({
@@ -6,23 +6,22 @@ import { CounterService } from '@app/services/counter.service';
     templateUrl: './counter.component.html',
     styleUrls: ['./counter.component.scss'],
 })
-export class CounterComponent implements OnInit, OnDestroy {
-    constructor(public counterService: CounterService) {
-    }
-    
+export class CounterComponent implements OnInit, AfterViewInit, OnDestroy {
+    constructor(public counterService: CounterService) {}
+
     count: number;
     player1: boolean;
     @Input() playerSide: boolean;
-    
+
     ngOnInit(): void {
         this.player1 = this.isPlayer1();
         this.counterService.resetCounter(this.player1);
         this.counterService.initializeCounter();
     }
 
-    ngOnDestroy(): void {
-        this.counterService.resetCounter(this.player1);
-    }
+    ngAfterViewInit(): void {}
+
+    ngOnDestroy(): void {}
 
     isPlayer1(): boolean {
         return sessionStorage.getItem('gameMaster') === sessionStorage.getItem('userName') ? true : false;
