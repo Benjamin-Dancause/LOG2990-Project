f97634779749b3a308b915e04133c53434527c2b
inikoloudis <inikoloudis@outlook.com>
2023-03-19 23:18:28 -0400

    topbar and sidebar are 100%

--- client/src/app/components/top-bar/top-bar.component.spec.ts
+++ client/src/app/components/top-bar/top-bar.component.spec.ts
@@ -1,18 +1,39 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CounterComponent } from '@app/components/counter/counter.component';
-import { TimerComponent } from '@app/components/timer/timer.component';
 import { TopBarComponent } from './top-bar.component';
+@Component({})
+class MockTimerComponent {
+    min = 0;
+    sec = 0;
+    minutes = '00';
+    seconds = '00';
+}
+@Component({})
+class MockCounterComponent {
+    count: number;
+    player1: boolean;
+    @Input() playerSide: boolean;
+}
 
-describe('TopBarComponent', () => {
+fdescribe('TopBarComponent', () => {
     let component: TopBarComponent;
     let fixture: ComponentFixture<TopBarComponent>;
+    let mockSessionStorage: any = {};
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [TopBarComponent, TimerComponent, CounterComponent],
+            declarations: [TopBarComponent, MockTimerComponent, MockCounterComponent],
             imports: [HttpClientTestingModule],
         }).compileComponents();
+
+        spyOn(sessionStorage, 'getItem').and.callFake((key: string): string => {
+            return mockSessionStorage[key] || null;
+        });
+
+        spyOn(sessionStorage, 'setItem').and.callFake((key: string, value: string): void => {
+            mockSessionStorage[key] = value;
+        });
 
         fixture = TestBed.createComponent(TopBarComponent);
         component = fixture.componentInstance;
@@ -25,28 +46,34 @@ describe('TopBarComponent', () => {
 
     it('should have userName', () => {
         const stubUserName = 'stubUserName';
-        spyOn(localStorage, 'getItem').and.returnValue(stubUserName);
-        spyOn(localStorage, 'setItem');
+        mockSessionStorage['userName'] = stubUserName;
 
-        localStorage.setItem('userName', stubUserName);
         component.ngOnInit();
-        expect(localStorage.getItem).toHaveBeenCalled();
+        expect(sessionStorage.getItem).toHaveBeenCalled();
         expect(component.userName).toEqual(stubUserName);
     });
 
     it('should be empty if no userName has been saved', () => {
         const stubUserName = null;
-        spyOn(localStorage, 'getItem').and.returnValue(stubUserName);
-        spyOn(localStorage, 'setItem');
+        mockSessionStorage['userName'] = stubUserName;
 
         component.ngOnInit();
-        expect(localStorage.getItem).toHaveBeenCalled();
+        expect(sessionStorage.getItem).toHaveBeenCalled();
         expect(component.userName).toEqual('');
     });
 
     it('should set userName to empty string if storedUserName is undefined or null', () => {
-        spyOn(localStorage, 'getItem').and.returnValue(null);
+        mockSessionStorage['userName'] = undefined;
         component.ngOnInit();
         expect(component.userName).toEqual('');
+    });
+
+    it('should set opponent to empty joiningPlayer if userName is same as gameMaster', () => {
+        component.single = false;
+        mockSessionStorage['userName'] = 'test1';
+        mockSessionStorage['gameMaster'] = 'test1';
+        mockSessionStorage['joiningPlayer'] = 'test2';
+        component.ngOnInit();
+        expect(component.opponent).toEqual('test2');
     });
 });
