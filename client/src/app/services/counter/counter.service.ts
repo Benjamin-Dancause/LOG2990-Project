507823ee148a2aec6eb7879abce3ee0716d5723a
Jérémy Langevin <jeremy.langevin@polymtl.ca>
2023-03-20 19:39:01 -0400

    Tests for counter service done

--- client/src/app/services/counter/counter.service.ts
+++ client/src/app/services/counter/counter.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { CommunicationService } from '../communication/communication.service';
 import { SocketService } from '../socket/socket.service';
@@ -6,7 +6,7 @@ import { SocketService } from '../socket/socket.service';
 @Injectable({
     providedIn: 'root',
 })
-export class CounterService implements OnInit {
+export class CounterService {
     counter: number = 0;
     counter2: number = 0;
     winCondition: number = 1000;
@@ -14,8 +14,6 @@ export class CounterService implements OnInit {
     allDiffsSubscription: Subscription;
 
     constructor(public socketService: SocketService, private communicationService: CommunicationService) {}
-
-    ngOnInit(): void {}
 
     initializeCounter(): void {
         const gameTitle: string = sessionStorage.getItem('gameTitle') as string;
@@ -58,6 +56,8 @@ export class CounterService implements OnInit {
     }
 
     ngOnDestroy() {
-        this.allDiffsSubscription.unsubscribe();
+        if(this.allDiffsSubscription) {
+            this.allDiffsSubscription.unsubscribe();
+        }
     }
 }
