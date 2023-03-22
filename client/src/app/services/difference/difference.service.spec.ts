53bf4e5f9432acbccedb8141c94c8c70cfbf6066
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-03-21 23:41:27 -0400

    reste 18 erreurs de lint

--- client/src/app/services/difference/difference.service.spec.ts
+++ client/src/app/services/difference/difference.service.spec.ts
@@ -59,8 +59,8 @@ describe('DifferenceService', () => {
 
     it('should return 1 when there is only 1 difference', () => {
         ctxStub1?.fillRect(0, 0, TEST_VALUE2, TEST_VALUE2);
-        const data1 = ctxStub1?.getImageData(0, 0, 640, 480);
-        const data2 = ctxStub2?.getImageData(0, 0, 640, 480);
+        const data1 = ctxStub1?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        const data2 = ctxStub2?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         const diff = service.findDifference(data1, data2, 0);
         expect(service.getDifference(diff).count).toEqual(1);
     });
@@ -68,15 +68,15 @@ describe('DifferenceService', () => {
     it('should return 2 when there is only 2 difference', () => {
         ctxStub1?.fillRect(0, 0, TEST_VALUE2, TEST_VALUE2);
         ctxStub2?.fillRect(TEST_VALUE3, TEST_VALUE3, TEST_VALUE2, TEST_VALUE2);
-        const data1 = ctxStub1?.getImageData(0, 0, 640, 480);
-        const data2 = ctxStub2?.getImageData(0, 0, 640, 480);
+        const data1 = ctxStub1?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        const data2 = ctxStub2?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         const diff = service.findDifference(data1, data2, 1);
         expect(service.getDifference(diff).count).toEqual(2);
     });
 
     it('should return 0 when there is no difference', () => {
-        const data1 = ctxStub1?.getImageData(0, 0, 640, 480);
-        const data2 = ctxStub2?.getImageData(0, 0, 640, 480);
+        const data1 = ctxStub1?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        const data2 = ctxStub2?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         const diff = service.findDifference(data1, data2, 1);
         expect(service.getDifference(diff).count).toEqual(0);
     });
@@ -84,24 +84,24 @@ describe('DifferenceService', () => {
     it('should return 1 when 2 differences are close', () => {
         ctxStub1?.fillRect(0, 0, TEST_VALUE2, TEST_VALUE2);
         ctxStub1?.fillRect(TEST_VALUE2, 0, TEST_VALUE2, TEST_VALUE2);
-        const data1 = ctxStub1?.getImageData(0, 0, 640, 480);
-        const data2 = ctxStub2?.getImageData(0, 0, 640, 480);
+        const data1 = ctxStub1?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        const data2 = ctxStub2?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         const diff = service.findDifference(data1, data2, TEST_VALUE1);
         expect(service.getDifference(diff).count).toEqual(1);
     });
 
     it('should return false when every pixel is different', () => {
         ctxStub1?.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        const data1 = ctxStub1?.getImageData(0, 0, 640, 480);
-        const data2 = ctxStub2?.getImageData(0, 0, 640, 480);
+        const data1 = ctxStub1?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        const data2 = ctxStub2?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         const diff = service.findDifference(data1, data2, 0);
         expect(service.isDifficult(diff)).toEqual(false);
     });
 
     it('should return false when there is only one small difference', () => {
         ctxStub1?.fillRect(0, 0, TEST_VALUE2, TEST_VALUE2);
-        const data1 = ctxStub1?.getImageData(0, 0, 640, 480);
-        const data2 = ctxStub2?.getImageData(0, 0, 640, 480);
+        const data1 = ctxStub1?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        const data2 = ctxStub2?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         const diff = service.findDifference(data1, data2, 0);
         expect(service.isDifficult(diff)).toEqual(false);
     });
@@ -114,8 +114,8 @@ describe('DifferenceService', () => {
         ctxStub1?.fillRect(0, TEST_VALUE2, 1, 1);
         ctxStub1?.fillRect(0, TEST_VALUE2, 1, 1);
         ctxStub1?.fillRect(0, TEST_VALUE2, 1, 1);
-        const data1 = ctxStub1?.getImageData(0, 0, 640, 480);
-        const data2 = ctxStub2?.getImageData(0, 0, 640, 480);
+        const data1 = ctxStub1?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        const data2 = ctxStub2?.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         const diff = service.findDifference(data1, data2, 0);
         expect(service.isDifficult(diff)).toEqual(false);
     });
