c27872105a22c934ea12b99e62ddd912220dfd3b
Jean-Christophe Pelletier <jean-christophe.pelletier@polymtl.ca>
2023-04-09 09:00:08 -0400

    test game config service done

--- server/app/services/game-config/game-config.service.spec.ts
+++ server/app/services/game-config/game-config.service.spec.ts
@@ -1,73 +1,39 @@
-import { promises as fs } from 'fs';
-// eslint-disable-next-line no-restricted-imports
-import { StoreService } from '../store/store.service';
-import { GameManager } from './game-config.service';
+import { GameConfigService } from './game-config.service';
 
-describe('GameManager', () => {
-    let gameManager: GameManager;
+fdescribe('GameConfigService', () => {
+    let gameConfigService: GameConfigService;
 
     beforeEach(() => {
-        const storeService = new StoreService();
-        gameManager = new GameManager(storeService);
+        gameConfigService = new GameConfigService();
     });
 
-    describe('createGame', () => {
-        it('should return the count from the gameData object', () => {
-            const gameData = { id: 1, count: 5, differences: [] };
-            // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-            expect(gameManager.createGame(gameData)).toBe(5);
-        });
+    it('should return the initial countdown time', async () => {
+        const countdownTime = await gameConfigService.getCountdownTime();
+        expect(countdownTime).toEqual(30);
     });
 
-    describe('verifyPos', () => {
-        it('should return an object indicating that no difference was found if the click is not on a difference', async () => {
-            const gameName = 'test_game';
-            const clickCoord = { x: 1, y: 2 };
-            const gameData = {
-                name: gameName,
-                differences: [
-                    [
-                        { x: 3, y: 4 },
-                        { x: 5, y: 6 },
-                    ],
-                    [
-                        { x: 7, y: 8 },
-                        { x: 9, y: 10 },
-                    ],
-                ],
-            };
-            const gamesContent = [{ name: gameName, differences: gameData.differences }];
-            jest.spyOn(fs, 'readFile').mockImplementation(async () => Promise.resolve(JSON.stringify(gamesContent)));
-
-            const expectedDifference = { isDifference: false, differenceNumber: 0, coords: [] };
-            const differenceInterface = await gameManager.verifyPos(gameName, clickCoord);
-            expect(differenceInterface).toEqual(expectedDifference);
-        });
+    it('should set the countdown time', () => {
+        gameConfigService.setCountdownTime(20);
+        expect(gameConfigService['countdownTime']).toEqual(20);
     });
 
-    describe('createGame', () => {
-        it('should return the amount of differences for games', async () => {
-            const gameData = {
-                id: 1,
-                count: 5,
-                differences: [
-                    [
-                        { x: 1, y: 2 },
-                        { x: 3, y: 4 },
-                    ],
-                    [
-                        { x: 5, y: 6 },
-                        { x: 7, y: 8 },
-                        { x: 9, y: 10 },
-                    ],
-                ],
-            };
-            const result = gameManager.createGame(gameData);
-            expect(result).toEqual(gameData.count);
-        });
+    it('should return the initial penalty time', async () => {
+        const penaltyTime = await gameConfigService.getPenaltyTime();
+        expect(penaltyTime).toEqual(5);
     });
 
-    afterEach(() => {
-        jest.restoreAllMocks();
+    it('should set the penalty time', () => {
+        gameConfigService.setPenaltyTime(10);
+        expect(gameConfigService['penaltyTime']).toEqual(10);
+    });
+
+    it('should return the initial time gained', async () => {
+        const timeGained = await gameConfigService.getTimeGained();
+        expect(timeGained).toEqual(5);
+    });
+
+    it('should set the time gained', () => {
+        gameConfigService.setTimeGained(10);
+        expect(gameConfigService['timeGained']).toEqual(10);
     });
 });
