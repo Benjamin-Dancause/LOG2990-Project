54e5cbd88fc3bf8b9ee583009733f490621fa188
ArcaneAmzek <charles.arsenault@polymtl.ca>
2023-04-10 21:12:02 -0400

    Fix un bug pour le multijouer temps lim

--- client/src/app/pages/waiting-room-limited-page/waiting-room-limited-page.component.ts
+++ client/src/app/pages/waiting-room-limited-page/waiting-room-limited-page.component.ts
@@ -45,7 +45,7 @@ export class WaitingRoomLimitedPageComponent implements OnInit, AfterViewInit {
     startLimitedTimeGame(): void {}
 
     leaveLobby() {
-        this.socketService.leaveLobby();
+        this.socketService.closeLobby(this.gameTitle);
         this.router.navigate(['/home']);
     }
 }
