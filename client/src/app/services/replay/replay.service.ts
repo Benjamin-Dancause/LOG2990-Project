017500eb8cbd171894a950ea13553a631144c401
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-04-16 18:38:43 -0400

    replay.service à 94.64%

--- client/src/app/services/replay/replay.service.ts
+++ client/src/app/services/replay/replay.service.ts
@@ -17,10 +17,10 @@ export class ReplayService {
     replayInterval: any;
     checkActionInterval: any;
     actionTime: number = 0;
+    speedSettings: number[] = [1, 2, TIME.FOUR_X_SPEED];
     private currentGameAction: GameAction;
-    private speedSettings: number[] = [1, 2, TIME.FOUR_X_SPEED];
 
-    constructor(private chat: ChatService, private canvasReplay: CanvasReplayService) {}
+    constructor(public chat: ChatService, public canvasReplay: CanvasReplayService) {}
 
     addAction(time: number, action: string, payload?: any): void {
         const gameAction: GameAction = { time, action, payload };
