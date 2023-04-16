8a67ca56e69ef554d23218cc55d6ba141caba7e6
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-04-16 15:50:50 -0400

    main-page.component à 100%

--- client/src/app/pages/main-page/main-page.component.spec.ts
+++ client/src/app/pages/main-page/main-page.component.spec.ts
@@ -1,12 +1,14 @@
 import { Location } from '@angular/common';
 import { HttpClientModule, HttpResponse } from '@angular/common/http';
 import { ComponentFixture, inject, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MainPageButtonComponent } from '@app/components/main-page-button/main-page-button.component';
 import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
 import { CommunicationService } from '@app/services/communication/communication.service';
+// import { SocketService } from '@app/services/socket/socket.service';
 import { of } from 'rxjs';
 import SpyObj = jasmine.SpyObj;
 
@@ -14,16 +16,26 @@ describe('MainPageComponent', () => {
     let component: MainPageComponent;
     let fixture: ComponentFixture<MainPageComponent>;
     let communicationServiceSpy: SpyObj<CommunicationService>;
+    // let socketServiceSpy: SpyObj<SocketService>;
 
     beforeEach(async () => {
         communicationServiceSpy = jasmine.createSpyObj('ExampleService', ['basicGet', 'basicPost']);
         communicationServiceSpy.basicGet.and.returnValue(of({ title: '', body: '' }));
         communicationServiceSpy.basicPost.and.returnValue(of(new HttpResponse<string>({ status: 201, statusText: 'Created' })));
 
+        // socketServiceSpy = jasmine.createSpyObj('SocketService', ['initializeSocket']);
+
         await TestBed.configureTestingModule({
-            imports: [RouterTestingModule.withRoutes([{ path: 'game-selection', component: GamePageComponent }]), HttpClientModule],
+            imports: [
+                RouterTestingModule.withRoutes([{ path: 'game-selection', component: GamePageComponent }]),
+                HttpClientModule,
+                MatDialogModule, // Ajout du module MatDialog
+            ],
             declarations: [MainPageComponent, MainPageButtonComponent],
-            providers: [{ provide: CommunicationService, useValue: communicationServiceSpy }],
+            providers: [
+                { provide: CommunicationService, useValue: communicationServiceSpy },
+                MatDialog, // Ajout de MatDialog
+            ],
         }).compileComponents();
     });
 
@@ -51,5 +63,36 @@ describe('MainPageComponent', () => {
     it("should have as title 'Le Jeu Des Différences'", () => {
         expect(typeof component.title).toEqual('string');
         expect(component.title).toEqual('Le Jeu Des Différences');
+    });
+
+    it('should open the dialog with correct game mode and title', () => {
+        spyOn(component.dialog, 'open');
+        component.gamemodeSelection();
+        expect(component.dialog.open).toHaveBeenCalledWith(component.gamemodeSelectionTemplate, {
+            width: '410px',
+        });
+        expect(sessionStorage.getItem('gameMode')).toEqual('tl');
+        expect(sessionStorage.getItem('gameTitle')).toEqual('Temps Limité');
+    });
+
+    describe('saveUserName', () => {
+        it('should save user name, game master and game mode in session storage and call initializeSocket', () => {
+            component.userName = 'John Doe';
+            component.saveUserName();
+            expect(sessionStorage.getItem('userName')).toEqual('John Doe');
+            expect(sessionStorage.getItem('gameMaster')).toEqual('John Doe');
+            expect(sessionStorage.getItem('gameMode')).toEqual('tl');
+            // expect(socketServiceSpy.initializeSocket).toHaveBeenCalled();
+        });
+    });
+
+    describe('saveUserNameDuo', () => {
+        it('should save user name and game mode in session storage and call initializeSocket', () => {
+            component.userName = 'Jane Doe';
+            component.saveUserNameDuo();
+            expect(sessionStorage.getItem('userName')).toEqual('Jane Doe');
+            expect(sessionStorage.getItem('gameMode')).toEqual('tl');
+            // expect(socketServiceSpy.initializeSocket).not.toHaveBeenCalled();
+        });
     });
 });
