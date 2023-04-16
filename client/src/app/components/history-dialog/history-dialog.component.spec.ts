90a7c7c492b00f83c4ef1599192607b2787eb636
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-04-16 14:20:12 -0400

    hitsoty-dialog à 58%

--- client/src/app/components/history-dialog/history-dialog.component.spec.ts
+++ client/src/app/components/history-dialog/history-dialog.component.spec.ts
@@ -2,10 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HistoryDialogComponent } from './history-dialog.component';
 
-describe('ConfirmationDialogComponent', () => {
+describe('HistoryDialogComponent', () => {
     let component: HistoryDialogComponent;
     let fixture: ComponentFixture<HistoryDialogComponent>;
-    // let dialogRef: MatDialogRef<HistoryDialogComponent>;
+    let dialogRef: MatDialogRef<HistoryDialogComponent>;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -21,11 +21,24 @@ describe('ConfirmationDialogComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(HistoryDialogComponent);
         component = fixture.componentInstance;
-        // dialogRef = TestBed.inject(MatDialogRef);
+        dialogRef = TestBed.inject(MatDialogRef);
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should display the correct time format', () => {
+        const time = 1234;
+        const expectedTime = '20:34'; // 20 minutes and 34 seconds
+        const result = component.displayTime(time);
+        expect(result).toEqual(expectedTime);
+    });
+
+    it('should close the dialog when onResetClick is called', () => {
+        spyOn(dialogRef, 'close');
+        component.onResetClick();
+        expect(dialogRef.close).toHaveBeenCalledWith('reset');
     });
 });
