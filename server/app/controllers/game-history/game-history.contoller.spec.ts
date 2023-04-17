dc5c03306276ee49fcf6e4ef9c6a285551819b8b
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-04-16 21:19:39 -0400

    game.history controller à 86%

--- server/app/controllers/game-history/game-history.contoller.spec.ts
+++ server/app/controllers/game-history/game-history.contoller.spec.ts
@@ -1,16 +1,51 @@
+import { databaseService } from '@app/services/database/database.service';
+import { gameHistoryInfo } from '@common/game-interfaces';
 import { Test } from '@nestjs/testing';
 import { GameHistoryController } from './game-history.controller';
 
-describe('DatabaseController', () => {
+describe('GameHistoryController', () => {
     let gameController: GameHistoryController;
+    let databaseServiceMock: Partial<databaseService>;
 
     beforeEach(async () => {
+        databaseServiceMock = {
+            getAllGameHistory: jest.fn(),
+            getGameHistory: jest.fn(),
+            createGameHistory: jest.fn(),
+            deleteAllGameHistory: jest.fn(),
+        };
+
         const app = await Test.createTestingModule({
             controllers: [GameHistoryController],
+            providers: [{ provide: databaseService, useValue: databaseServiceMock }],
         }).compile();
 
         gameController = app.get<GameHistoryController>(GameHistoryController);
     });
 
+    describe('createGameHistory', () => {
+        it('should call databaseService.createGameHistory with the provided gameHistoryInfo', () => {
+            const gameHistoryInfo: gameHistoryInfo = {
+                gameTitle: 'game1',
+                winner: 'joueur 1',
+                loser: 'joueur 2',
+                surrender: false,
+                time: { startTime: 'string', duration: 3 },
+                isSolo: false,
+                isLimitedTime: false,
+            };
 
+            gameController.createGameHistory(gameHistoryInfo);
+
+            expect(databaseServiceMock.createGameHistory).toHaveBeenCalledWith(gameHistoryInfo);
+        });
+    });
+
+    describe('deleteHistory', () => {
+        it('should call databaseService.deleteAllGameHistory', () => {
+            gameController.deleteHistory();
+
+            expect(databaseServiceMock.deleteAllGameHistory).toHaveBeenCalled();
+        });
+    });
 });
