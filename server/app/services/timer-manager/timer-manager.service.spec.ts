79ee04111b996735dc923a4253b83ee4190004da
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-04-16 22:39:17 -0400

    timer-manager à 55%

--- server/app/services/timer-manager/timer-manager.service.spec.ts
+++ server/app/services/timer-manager/timer-manager.service.spec.ts
@@ -1,98 +1,57 @@
 import { ClassicModeGateway } from '@app/gateways/timer/classic-mode.gateway';
 import { DELAY } from '@common/constants';
-import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TimerManagerService } from './timer-manager.service';
 
 describe('TimerManagerService', () => {
     let service: TimerManagerService;
-    let app: INestApplication;
-    let timerGateway: ClassicModeGateway;
+    let classicModeGateway: ClassicModeGateway;
 
     beforeEach(async () => {
-        jest.useFakeTimers();
-
         const module: TestingModule = await Test.createTestingModule({
             providers: [
                 TimerManagerService,
                 {
                     provide: ClassicModeGateway,
-                    useFactory: () => ({
+                    useValue: {
                         emitTimeToRoom: jest.fn(),
-                    }),
+                    },
                 },
             ],
         }).compile();
 
         service = module.get<TimerManagerService>(TimerManagerService);
-        timerGateway = module.get<ClassicModeGateway>(ClassicModeGateway);
-
-        app = module.createNestApplication();
-        await app.init();
+        classicModeGateway = module.get<ClassicModeGateway>(ClassicModeGateway);
     });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
+    afterEach(() => {
+        jest.clearAllMocks();
     });
 
-    afterEach(async () => {
-        jest.clearAllTimers();
-        await app.close();
+    it('start timer should start a timer with the correct delay', () => {
+        const roomId = 'test-room';
+        const gameMode = 'tl';
+
+        jest.useFakeTimers();
+
+        service.startTimer(roomId, gameMode);
+
+        jest.advanceTimersByTime(DELAY.SMALLTIMEOUT * 2);
+
+        expect(classicModeGateway.emitTimeToRoom).toHaveBeenCalledTimes(2);
+        expect(classicModeGateway.emitTimeToRoom).toHaveBeenCalledWith(roomId, expect.any(Number));
     });
 
-    describe('startTimer', () => {
-        it('should start a timer for a room', () => {
-            service.startTimer('testRoom');
-            expect(service.getTimeFromRoom('testRoom')).toEqual(0);
-        });
+    it('should decrement the timer if game mode is tl', () => {
+        const roomId = 'test-room';
+        const gameMode = 'tl';
+        const initialTime = 60;
 
-        it('should not start a timer for a room that already has one timer', () => {
-            service.startTimer('testRoom');
-            const intervalId = service['intervals'].get('testRoom');
-            service.startTimer('testRoom');
-            expect(service['intervals'].get('testRoom')).toEqual(intervalId);
-        });
-    });
+        service.startTimer(roomId, gameMode);
+        service.updateTimer(roomId, gameMode);
 
-    describe('updateTimer', () => {
-        it('should increment the time and emit to the room', () => {
-            service['timers'].set('testRoom', 0);
-            service.updateTimer('testRoom');
-            expect(timerGateway.emitTimeToRoom).toHaveBeenCalledWith('testRoom', 1);
-            expect(service['timers'].get('testRoom')).toEqual(1);
-        });
-    });
-
-    describe('deleteTimerData', () => {
-        it('should delete the timer data for a room', () => {
-            service['timers'].set('testRoom', 0);
-            // const intervalId = service['intervals'].get('testRoom');
-            service.deleteTimerData('testRoom');
-            expect(service['intervals'].has('testRoom')).toBeFalsy();
-            expect(service['timers'].has('testRoom')).toBeFalsy();
-            // expect(clearInterval).toHaveBeenCalledWith(intervalId);
-        });
-    });
-
-    describe('resetTimer', () => {
-        it('should reset the timer for a room', () => {
-            const TIME = 10;
-            service['timers'].set('testRoom', TIME);
-            // const intervalId = service['intervals'].get('testRoom');
-            service.resetTimer('testRoom');
-            // expect(service['timers'].get('testRoom')).toEqual(0);
-            expect(service['intervals'].has('testRoom')).toBeFalsy();
-            // expect(clearInterval).toHaveBeenCalledWith(intervalId);
-        });
-    });
-
-    describe('setInterval', () => {
-        it('should call updateTimer function every 1000ms', () => {
-            const updateTimerSpy = jest.spyOn(service, 'updateTimer');
-            service.startTimer('testRoom');
-            jest.advanceTimersByTime(DELAY.HUGE_TIMEOUT);
-            // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-            expect(updateTimerSpy).toHaveBeenCalledTimes(5);
-        });
+        expect(service.getTimeFromRoom(roomId, gameMode)).toEqual(initialTime - 1);
+        expect(classicModeGateway.emitTimeToRoom).toHaveBeenCalledTimes(1);
+        expect(classicModeGateway.emitTimeToRoom).toHaveBeenCalledWith(roomId, initialTime - 1);
     });
 });
