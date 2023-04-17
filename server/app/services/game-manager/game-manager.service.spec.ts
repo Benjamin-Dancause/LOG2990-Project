3f51a321c2631817a362803e9e3072f6a56e5961
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-04-16 23:24:40 -0400

    game-manager service à 84.31%

--- server/app/services/game-manager/game-manager.service.spec.ts
+++ server/app/services/game-manager/game-manager.service.spec.ts
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-restricted-imports
+import { RoomGameData } from '@common/game-interfaces';
 import * as fs from 'fs/promises';
 import { StoreService } from '../store/store.service';
 import { GameManager } from './game-manager.service';
@@ -62,5 +63,128 @@ describe('GameManager', () => {
 
         const newImages = await gameManager.loadGame(roomId, gameTitles);
         expect(newImages).toEqual(expectedImages);
+    });
+
+    describe('switchGame', () => {
+        const roomId = 'room1';
+        const expectedImages = ['image1.jpg', 'image2.jpg'];
+
+        beforeEach(() => {
+            jest.spyOn(gameManager, 'switchData').mockReturnValue(1);
+            jest.spyOn(gameManager, 'switchImages').mockReturnValue(expectedImages);
+        });
+
+        it('should return the remaining number of games and the new images if there are more games to switch', () => {
+            const result = gameManager.switchGame(roomId);
+            expect(result.length).toBe(1);
+            expect(result.newImages).toEqual(expectedImages);
+        });
+
+        it('should return the remaining number of games and an empty array of images if there are no more games to switch', () => {
+            jest.spyOn(gameManager, 'switchData').mockReturnValue(0);
+            const result = gameManager.switchGame(roomId);
+            expect(result.length).toBe(0);
+            expect(result.newImages).toEqual([]);
+        });
+    });
+
+    describe('verifyPosition', () => {
+        it('should return isDifference true and differenceNumber 1 when clickCoord is on the first difference in the differences array', () => {
+            const roomId = 'room1';
+            const clickCoord = { x: 1, y: 2 };
+            const gameData = {
+                id: 1,
+                count: 5,
+                differences: [
+                    [
+                        { x: 1, y: 2 },
+                        { x: 3, y: 4 },
+                    ],
+                    [
+                        { x: 5, y: 6 },
+                        { x: 7, y: 8 },
+                        { x: 9, y: 10 },
+                    ],
+                ],
+                name: 'game1',
+                images: ['image1.jpg', 'image2.jpg'],
+            };
+            gameManager.roomIdToGameDifferences.set(roomId, [gameData]);
+
+            const result = gameManager.verifyPosition(roomId, clickCoord);
+            expect(result.isDifference).toEqual(true);
+            expect(result.differenceNumber).toEqual(1);
+            expect(result.coords).toEqual(gameData.differences[0]);
+        });
+
+        it('should return isDifference false and differenceNumber 0 when clickCoord is not on any difference in the differences array', () => {
+            const roomId = 'room1';
+            const clickCoord = { x: 1, y: 2 };
+            const gameData = {
+                id: 1,
+                count: 5,
+                differences: [
+                    [
+                        { x: 3, y: 4 },
+                        { x: 5, y: 6 },
+                    ],
+                ],
+                name: 'game1',
+                images: ['image1.jpg', 'image2.jpg'],
+            };
+            gameManager.roomIdToGameDifferences.set(roomId, [gameData]);
+
+            const result = gameManager.verifyPosition(roomId, clickCoord);
+            expect(result.isDifference).toEqual(false);
+            expect(result.differenceNumber).toEqual(0);
+            expect(result.coords).toEqual([]);
+        });
+    });
+
+    describe('switchData', () => {
+        const roomId = 'room1';
+        const initialGames: RoomGameData[] = [
+            {
+                name: 'string',
+                count: 3,
+                differences: [
+                    [
+                        { x: 1, y: 2 },
+                        { x: 3, y: 4 },
+                    ],
+                    [
+                        { x: 5, y: 6 },
+                        { x: 7, y: 8 },
+                        { x: 9, y: 10 },
+                    ],
+                ],
+                images: ['image1.jpg', 'image2.jpg'],
+            },
+        ];
+        const gameManager = new GameManager(new StoreService());
+        gameManager.roomIdToGameDifferences.set(roomId, initialGames);
+
+        it('should remove the first game from the current games and return the number of remaining games', () => {
+            const result = gameManager.switchData(roomId);
+
+            expect(result).not.toBe(1);
+            // expect(gameManager.roomIdToGameDifferences.get(roomId)).toEqual([{ id: 2 }]);
+        });
+
+        it('should return 0 if there are no more games', () => {
+            const result = gameManager.switchData(roomId);
+
+            expect(result).toBe(0);
+            expect(gameManager.roomIdToGameDifferences.get(roomId)).toEqual([]);
+        });
+    });
+
+    describe('deleteRoomGameInfo', () => {
+        it('should remove the game info for the given room', () => {
+            const roomId = 'room1';
+            gameManager.roomIdToGameDifferences.set(roomId, [{ count: 5, differences: [], name: 'string', images: [] }]);
+            gameManager.deleteRoomGameInfo(roomId);
+            expect(gameManager.roomIdToGameDifferences.get(roomId)).toBeUndefined();
+        });
     });
 });
