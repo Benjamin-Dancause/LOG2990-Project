d97fd1ff0cfe068cea0d84d0cda1799c91cbeaca
YuliiaOz <90001248+YuliiaOz@users.noreply.github.com>
2023-04-16 21:25:49 -0400

    game-history.controller à 100%

--- server/app/controllers/game-history/game-history.contoller.spec.ts
+++ server/app/controllers/game-history/game-history.contoller.spec.ts
@@ -23,6 +23,53 @@ describe('GameHistoryController', () => {
         gameController = app.get<GameHistoryController>(GameHistoryController);
     });
 
+    describe('getAllHistory', () => {
+        it('should call databaseService.getAllGameHistory', async () => {
+            const expectedGameHistory: gameHistoryInfo[] = [
+                {
+                    gameTitle: 'game1',
+                    winner: 'joueur 1',
+                    loser: 'joueur 2',
+                    surrender: false,
+                    time: { startTime: 'string', duration: 3 },
+                    isSolo: false,
+                    isLimitedTime: false,
+                },
+            ];
+
+            databaseServiceMock.getAllGameHistory = jest.fn(async () => Promise.resolve(expectedGameHistory));
+
+            const result = await gameController.getAllHistory();
+
+            expect(databaseServiceMock.getAllGameHistory).toHaveBeenCalled();
+            expect(result).toEqual(expectedGameHistory);
+        });
+    });
+
+    describe('getHistory', () => {
+        it('should call databaseService.getGameHistory with the provided gameTitle', async () => {
+            const gameTitle = 'game1';
+            const expectedGameHistory: gameHistoryInfo[] = [
+                {
+                    gameTitle: 'game1',
+                    winner: 'joueur 1',
+                    loser: 'joueur 2',
+                    surrender: false,
+                    time: { startTime: 'string', duration: 3 },
+                    isSolo: false,
+                    isLimitedTime: false,
+                },
+            ];
+
+            databaseServiceMock.getGameHistory = jest.fn(async () => Promise.resolve(expectedGameHistory));
+
+            const result = await gameController.getHistory(gameTitle);
+
+            expect(databaseServiceMock.getGameHistory).toHaveBeenCalledWith(gameTitle);
+            expect(result).toEqual(expectedGameHistory);
+        });
+    });
+
     describe('createGameHistory', () => {
         it('should call databaseService.createGameHistory with the provided gameHistoryInfo', () => {
             const gameHistoryInfo: gameHistoryInfo = {
